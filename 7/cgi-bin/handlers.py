47f36379cad08e2d258aedf16ef2c9d438b73e2c
romak <romaknyazev55@yandex.ru>
2022-12-16 04:40:01 +0300

    1

--- 7/cgi-bin/handlers.py
+++ 7/cgi-bin/handlers.py
@@ -24,17 +24,19 @@ with open('names.txt', 'w') as names_file:
     names_file.write(phone_field + '\n')
 
 with open('values.txt', 'w') as values_file:
-    values_file.write(name or '' + '\n')
-    values_file.write(university or '' + '\n')
-    values_file.write(phone or '' + '\n')
+    values_file.write((name or '') + '\n')
+    values_file.write((university or '') + '\n')
+    values_file.write((phone or '') + '\n')
 
 names_file_size = os.path.getsize('names.txt')
 values_file_size = os.path.getsize('values.txt')
-print(f'Длина файла "names.txt": {names_file_size}')
-print(f'Длина файла "values.txt": {values_file_size}')
+print(f'Длина файла "names.txt": {names_file_size}<br>')
+print(f'Длина файла "values.txt": {values_file_size}<br>')
+
 
 with open('values.txt', 'r') as values_file:
-    print(f'Содержимое файла "values.txt": {values_file.read()}')
+    print(f'Содержимое файла "values.txt":<br>')
+    print(values_file.read().replace('\n', '<br>'))
 
 sequence = bytes(range(25))
 with open('binary_data.dat', 'wb') as binary_file:
@@ -42,16 +44,16 @@ with open('binary_data.dat', 'wb') as binary_file:
 
 with open('binary_data.dat', 'rb') as binary_file:
     data = binary_file.read()
-    print(f'Значение 20-го байта: {data[20]}')
+    print(f'Значение 20-го байта: {data[20]}<br>')
     try:
         binary_file.seek(-4, 2)
-        print(f'Значение трех байтов: {binary_file.read(3)}')
+        print(f'Значение трех байтов: {binary_file.read(3)}<br>')
     except OSError:
-        print('Недопустимые значения для "offset" и "whence"')
+        print('Недопустимые значения для "offset" и "whence"<br>')
 
     digit = str(data[-1])[-1]
     try:
         assert digit == 3
-        print(f'Последняя цифра {digit} в последнем числе последовательности совпадает с целевой')
+        print(f'Последняя цифра {digit} в последнем числе последовательности совпадает с целевой<br>')
     except AssertionError:
-        print(f'Последняя цифра {digit} в последнем числе последовательности не совпадает с целевой')
+        print(f'Последняя цифра {digit} в последнем числе последовательности не совпадает с целевой<br>')
